44a9cbfd931fc962efa0d9dc857d921e16db4a3a
Haha <Haha_Email@163.com>
2021-12-13 17:57:04 +0800

    1.优化build.gradle中依赖方式，可以看到库新版本更新提示 2.移除暂时不需要的库

--- base-common/src/main/java/me/goldze/mvvmhabit/binding/viewadapter/view/ViewAdapter.java
+++ base-common/src/main/java/me/goldze/mvvmhabit/binding/viewadapter/view/ViewAdapter.java
@@ -2,12 +2,8 @@ package me.goldze.mvvmhabit.binding.viewadapter.view;
 
 import android.view.View;
 
-import com.jakewharton.rxbinding2.view.RxView;
-
-import java.util.concurrent.TimeUnit;
-
 import androidx.databinding.BindingAdapter;
-import io.reactivex.functions.Consumer;
+
 import me.goldze.mvvmhabit.binding.command.BindingCommand;
 
 /**
@@ -15,8 +11,6 @@ import me.goldze.mvvmhabit.binding.command.BindingCommand;
  */
 
 public class ViewAdapter {
-    //防重复点击间隔(秒)
-    public static final int CLICK_INTERVAL = 1;
 
     /**
      * requireAll 是意思是是否需要绑定全部参数, false为否
@@ -27,43 +21,15 @@ public class ViewAdapter {
     @BindingAdapter(value = {"onClickCommand", "isThrottleFirst"}, requireAll = false)
     public static void onClickCommand(View view, final BindingCommand clickCommand, final boolean isThrottleFirst) {
         if (isThrottleFirst) {
-            RxView.clicks(view)
-                    .subscribe(new Consumer<Object>() {
-                        @Override
-                        public void accept(Object object) throws Exception {
-                            if (clickCommand != null) {
-                                clickCommand.execute();
-                            }
-                        }
-                    });
+            //TODO:
+            if (clickCommand != null) {
+                clickCommand.execute();
+            }
         } else {
-            RxView.clicks(view)
-                    .throttleFirst(CLICK_INTERVAL, TimeUnit.SECONDS)//1秒钟内只允许点击1次
-                    .subscribe(new Consumer<Object>() {
-                        @Override
-                        public void accept(Object object) throws Exception {
-                            if (clickCommand != null) {
-                                clickCommand.execute();
-                            }
-                        }
-                    });
+            if (clickCommand != null) {
+                clickCommand.execute();
+            }
         }
-    }
-
-    /**
-     * view的onLongClick事件绑定
-     */
-    @BindingAdapter(value = {"onLongClickCommand"}, requireAll = false)
-    public static void onLongClickCommand(View view, final BindingCommand clickCommand) {
-        RxView.longClicks(view)
-                .subscribe(new Consumer<Object>() {
-                    @Override
-                    public void accept(Object object) throws Exception {
-                        if (clickCommand != null) {
-                            clickCommand.execute();
-                        }
-                    }
-                });
     }
 
     /**
@@ -118,16 +84,4 @@ public class ViewAdapter {
             view.setVisibility(View.GONE);
         }
     }
-//    @BindingAdapter({"onTouchCommand"})
-//    public static void onTouchCommand(View view, final ResponseCommand<MotionEvent, Boolean> onTouchCommand) {
-//        view.setOnTouchListener(new View.OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View v, MotionEvent event) {
-//                if (onTouchCommand != null) {
-//                    return onTouchCommand.execute(event);
-//                }
-//                return false;
-//            }
-//        });
-//    }
 }
